cfab120724f0611588bd6d7ee31a274bd761a87b
isnaeky <isnaeky.treiny@gmail.com>
2021-03-28 13:00:26 -0600

    Update environment.ts
    Delete API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAj_wRu0nFFxQeiMseLVrVTJg_kVR9f2HE",
+    apiKey: "You_api_key",
     authDomain: "uploadfotos-2451.firebaseapp.com",
     databaseURL: "https://uploadfotos-2451-default-rtdb.firebaseio.com",
     projectId: "uploadfotos-2451",
